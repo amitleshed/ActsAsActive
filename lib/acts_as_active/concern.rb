03ba2ffae0b00c8865cc293af7be855549e0a982
Amit Leshed <amitleshed@icloud.com>
2025-08-06 12:21:20 -0400

    record activity works

--- lib/acts_as_active/concern.rb
+++ lib/acts_as_active/concern.rb
@@ -2,22 +2,26 @@ module ActsAsActive
   extend ActiveSupport::Concern
 
   class_methods do
+    SUPPORTED_EVENTS = %i[create update destroy].freeze
+
     def acts_as_active(options = {})
       has_many :activities, as: :trackable, class_name: "Activity"
-      events = Array(options[:on] || [:create, :update, :destroy])
-      supported = %i[create update destroy]
-      
-      events.each do |event|
-        next unless supported.include?(event)
-        after_commit -> { record_activity(event) }, on: event
+
+      events = Array(options[:on] || SUPPORTED_EVENTS) & SUPPORTED_EVENTS
+      events.each do |ev|
+        after_commit on: ev do |_ignored|
+          record_activity!
+        end
       end
     end
   end
 
   private
 
-  def record_activity(event)
-    Rails.logger.info "[ActsAsActive] Recorded #{event} for #{self.inspect}"
-    self.activities.create(occurred_on: Time.now.to_date)
+  def record_activity!(at: Time.current)
+    today = at.to_date
+    activity = activities.find_or_initialize_by(occurred_on: today)
+    activity.count = (activity.count || 0) + 1
+    activity.save!
   end
 end
